153aff853a4505bf570b5f336337254cacd2490b
Marcin Knara <marcinknara@gmail.com>
2024-11-30 11:52:38 -0500

    1.0.4

--- main_app.py
+++ main_app.py
@@ -7,16 +7,24 @@ from tkinter import messagebox
 import sys
 
 # Determine the base path of the application
-BASE_PATH = os.path.dirname(os.path.abspath(__file__))
+if getattr(sys, 'frozen', False):
+    # If running from a macOS .app bundle
+    if sys.platform == "darwin" and ".app" in os.path.abspath(sys.executable):
+        BASE_PATH = os.path.abspath(os.path.join(sys.executable, "../../../.."))
+    else:
+        # If running from the one-folder mode
+        BASE_PATH = os.path.dirname(os.path.abspath(sys.executable))
+else:
+    # If running directly as a Python script
+    BASE_PATH = os.path.dirname(os.path.abspath(__file__))
 
 # Paths for local version file and updater executable
 LOCAL_VERSION_FILE = os.path.join(BASE_PATH, "version.json")
 
-# Path to the updater executable
+# Path to the updater folder
+UPDATER_EXECUTABLE = os.path.join(BASE_PATH, "update_manager", "update_manager")
 if sys.platform == "win32":
-    UPDATER_EXECUTABLE = os.path.join(BASE_PATH, "..", "update_manager", "update_manager.exe")
-else:
-    UPDATER_EXECUTABLE = os.path.join(BASE_PATH, "..", "update_manager", "update_manager")
+    UPDATER_EXECUTABLE += ".exe"
 
 GITHUB_VERSION_URL = "https://raw.githubusercontent.com/marcinknara/auto-update-python/main/version.json"
 
@@ -34,6 +42,7 @@ def check_for_updates():
         # Compare versions and trigger the updater if needed
         if remote_version > local_version:
             if messagebox.askyesno("Update Available", "A new version is available. Update now?"):
+                # Launch the updater executable
                 subprocess.Popen([UPDATER_EXECUTABLE])
                 root.destroy()  # Close the main application
         else:
@@ -52,6 +61,6 @@ settings_menu.add_command(label="Check for Updates", command=check_for_updates)
 menubar.add_cascade(label="Settings", menu=settings_menu)
 root.config(menu=menubar)
 
-tk.Label(root, text="Welcome to the Minimal Updater App! v1.0.3").pack(pady=20)
+tk.Label(root, text="Welcome to the Minimal Updater App! v1.0.4").pack(pady=20)
 root.geometry("400x200")
 root.mainloop()
